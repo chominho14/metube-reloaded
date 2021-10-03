34904890021423fd0313ef166e0bfb2f465bf79a
chominho14 <chominho14@naver.com>
2021-10-03 15:28:09 +0900

    v3

--- src/server.js
+++ src/server.js
@@ -1,24 +1,36 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger)
 
-const gossipmiddleware =(req, res, next) =>{
-    console.log("dffef");
-    next();
-};
 
-const handleHome = (req, res) =>{
-    return res.send("wow this is middle");
-};
+const globalRouter = express.Router();
 
-const handleLogin = (req,res) =>{
-    return res.send("login start");
-};
+const handleHome = (req, res) => res.send("Home");
 
-app.use(gossipmiddleware);
-app.get("/",handleHome );
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+
+app.use("/",globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 
 const handleListening = () => 
     console.log(`Server listening on port 4000.http://localhost:${PORT}/`);
