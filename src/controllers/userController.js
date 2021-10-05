2ae06597664143b8ec3380f853d93409a6cb1be5
chominho14 <chominho14@naver.com>
2021-10-05 22:16:56 +0900

    3.2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,4 +2,10 @@ export const join = (req, res) => res.sent("join");
 
 export const remove = (req, res ) => res.send("Remove User");
 export const edit = (req, res) => res.send("Edit User");
+export const login = (req, res) => res.sent("login");
+export const logout = (req, res) => res.sent("logout");
+export const see = (req, res) => res.sent("See User");
+
+
+
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,8 @@
 export const trending = (req, res) => res.send("Home Page Vidies");
 
-export const watch = (req, res ) => res.send("watch");
+export const see = (req, res ) => res.send("See Videos");
 export const edit = (req, res) => res.send("edit");
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req,res) => res.send("Delete Videos");
 
