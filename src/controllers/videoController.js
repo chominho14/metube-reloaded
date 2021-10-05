072d32df6976d7df0a98cd0fc1c83ddd510dd811
chominho14 <chominho14@naver.com>
2021-10-05 23:25:18 +0900

    v3.3

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
 export const trending = (req, res) => res.send("Home Page Vidies");
 
-export const see = (req, res ) => res.send("See Videos");
+export const see = (req, res ) => {
+    console.log(res.params);
+    return res.send(`See Videos : #${req.params.id}`);
+};
 export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
