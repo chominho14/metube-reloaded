45cd6fd881dda7a00eb4da48673284f29ff92a7e
chominho14 <chominho14@naver.com>
2021-10-08 16:50:38 +0900

    v4.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,14 @@
-export const trending = (req, res) => res.render("home");
+const fakeUser = {
+    username : "mino",
+    loggedIn : false,
+};
 
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return res.render("home", {pageTitle : "Home", fakeUser,videos });
+};
 export const see = (req, res ) => res.render("watch");
-export const edit = (req, res) => res.send("edit");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req,res) => res.send("Delete Videos");
