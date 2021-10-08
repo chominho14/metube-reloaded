77f7cadb2a6b4bf4c8c945f5d6a42de10cbd9804
chominho14 <chominho14@naver.com>
2021-10-08 18:42:00 +0900

    v4.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,39 @@
-const fakeUser = {
-    username : "mino",
-    loggedIn : false,
-};
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 
 export const trending = (req, res) => {
-    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    return res.render("home", {pageTitle : "Home", fakeUser,videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res ) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req,res) => res.send("Delete Videos");
-
+export const deleteVideo = (req, res) => res.send("Delete Videos");
