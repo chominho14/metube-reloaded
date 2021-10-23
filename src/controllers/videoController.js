f754835f224387e9ee8990d9424b1e29ff39d30b
chominho14 <chominho14@naver.com>
2021-10-24 00:07:11 +0900

    v5.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,30 +1,3 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
--- src/db.js
+++ src/db.js
@@ -8,8 +8,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
 const db = mongoose.connection;
 
 const handelOpen = () => console.log("Connected to DB");
-const handleError =  (error) => console.log("DB Error", error));
+const handleError = (error) => console.log("DB Error", error);
 
 db.on("error", handleError);
 db.once("open", handelOpen);
-
