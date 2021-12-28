e9214410a90b56e42b09031492e5e4630d1d2b39
chominho14 <chominho14@naver.com>
2021-12-28 15:11:55 +0900

    v6.7

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,6 +5,7 @@ export const localsMiddleware = (req, res, next) => {
   next();
 };
 
+// 로그인 되어 있는 사람들만 이용 가능하도록 보호하는 미들웨어
 export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
     next();
@@ -13,6 +14,7 @@ export const protectorMiddleware = (req, res, next) => {
   }
 };
 
+// 로그인 되어 있는 사람들만 홈으로 이동시키는 미들웨어
 export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
