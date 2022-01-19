1efe9eff5ffdf4d12f4816a036aefeb6cc8c6205
chominho14 <chominho14@naver.com>
2022-01-19 17:37:51 +0900

    v12.3(Comments, Router and Controllers work)

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,13 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = event => {
   // 작업을 잠시 멈춰준다.
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      text,
+    },
+  });
 };
 
-btn.addEventListener("submit", handleSubmit);
+if (form) {
+  btn.addEventListener("submit", handleSubmit);
+}
