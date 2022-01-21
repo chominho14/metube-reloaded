8b53d2b4f22254a048c420c6188d98904c88a99f
chominho14 <chominho14@naver.com>
2022-01-21 17:15:22 +0900

    s3 uploads -6.2

--- src/middlewares.js
+++ src/middlewares.js
@@ -10,6 +10,8 @@ const s3 = new aws.S3({
   },
 });
 
+const isHeroku = process.env.NODE_ENV === "production";
+
 const s3ImageUploader = multerS3({
   s3,
   bucket: "metubee/images",
@@ -34,6 +36,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Metube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -62,7 +65,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: s3ImageUploader,
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -70,5 +73,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: s3VideoUploader,
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
