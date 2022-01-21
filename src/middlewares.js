0cc5813a9f833517c0ceafea00e2cf45c3c57ee8
chominho14 <chominho14@naver.com>
2022-01-21 17:24:11 +0900

    s3 uploads -6.4

--- src/middlewares.js
+++ src/middlewares.js
@@ -30,6 +30,7 @@ const s3VideoUploader = multerS3({
       "s3:x-amz-acl": ["public-read"],
     },
   },
+  contentType: multerS3.AUTO_CONTENT_TYPE,
 });
 
 export const localsMiddleware = (req, res, next) => {
