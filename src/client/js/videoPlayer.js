b6ca5f101683aa45f19771a664a89d58386245ce
chominho14 <chominho14@naver.com>
2022-03-22 23:46:31 +0900

    Video Time work 6

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -67,7 +67,7 @@ const handleVolumeChange = event => {
 };
 
 const formatTime = seconds => {
-  new Date(seconds * 1000).toISOString().substring(14, 19);
+  new Date(seconds * 1000).toISOString().substring(11, 19);
 };
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
