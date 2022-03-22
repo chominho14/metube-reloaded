ca696c059c5c29aa29445aadc8de23883f9589f0
chominho14 <chominho14@naver.com>
2022-03-22 23:08:55 +0900

    Video Time work 2)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -137,7 +137,7 @@ const handleEnded = () => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("loadeddata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullscreen);
