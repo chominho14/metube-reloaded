db3ef2f25e5769b57ad3c48bb62ab0d49bc6de26
chominho14 <chominho14@naver.com>
2022-03-22 23:21:00 +0900

    Video Time work 3

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -67,11 +67,7 @@ const handleVolumeChange = event => {
 };
 
 const formatTime = seconds => {
-  if (seconds >= 3600) {
-    return new Date(seconds * 1000).toISOString().substring(11, 19);
-  } else {
-    return new Date(seconds * 1000).toISOString().substring(14, 19);
-  }
+  new Date(seconds * 1000).toISOString().substring(14, 19);
 };
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
