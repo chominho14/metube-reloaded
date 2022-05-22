d70a7ea4114d4e52f0fb8cbd00ba43815e26bb74
chominho14 <chominho14@naver.com>
2022-05-22 21:33:48 +0900

    Recording Audio - 3

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -104,7 +104,7 @@ const handleStart = () => {
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
+    audio: true,
     video: {
       width: 1024,
       height: 576,
